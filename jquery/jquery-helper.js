4cd38723f3480a400977738290310fdcacca5441
mins01 <mins01@lycos.co.kr>
2015-04-26 01:54:33 +0900

    + jquery etc function

---
+++ jquery/jquery-helper.js
@@ -0,0 +1,19 @@
+/**
+* jquery에서 지원 안되는 것들 추가적으로 
+*/
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
