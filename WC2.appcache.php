0eb0f0a00eac3681dbab35e111d77e9e106dc78f
mins01 <mins01.lycos.co.kr@gmail.com>
2017-08-20 16:52:19 +0900

    행상캐싱하도록

--- WC2.appcache.php
+++ WC2.appcache.php
@@ -4,4 +4,4 @@ header("Cache-Control: no-cache,must-revalidate");
 header('Content-Type: text/cache-manifest');
 
 require('WC2.appcache');
-echo "\n\n#".time();
+echo "\n\n#".time().str_repeat('m',date('n')).str_repeat('d',date('j')).str_repeat('h',date('G')).str_repeat('h',date('G')).str_repeat('_',rand(0,60));
