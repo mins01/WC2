e518a0b6fb7456c85dc915936a6d66a455a2ab85
mins01 <mins01.lycos.co.kr@gmail.com>
2017-08-20 16:58:50 +0900

    앱캐시 갱신 방법 수정.

--- WC2.appcache.php
+++ WC2.appcache.php
@@ -1,7 +1,22 @@
 <?
+// 과거의 날짜
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
+// 항상 변경됨
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+
+// HTTP/1.1
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+
+// HTTP/1.0
 header("Pragma: no-cache");
-header("Cache-Control: no-cache,must-revalidate");
+
 header('Content-Type: text/cache-manifest');
 
 require('WC2.appcache');
-echo "\n\n#".time();
+
+$t_m_ = filemtime('./');
+$t_m_js = filemtime('./js');
+$t_m_css = filemtime('./css');
+echo "\n\n#".date('Y-m-d H:i:s,',$t_m_).date('Y-m-d H:i:s,',$t_m_js).date('Y-m-d H:i:s,',$t_m_css);
