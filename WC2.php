d2fbd11569c1964556b38ba0c5f4e953e30a6743
공대여자 <mins01.lycos.co.kr@gmail.com>
2018-12-18 13:04:31 +0900

    오타 수정.

--- WC2.php
+++ WC2.php
@@ -2241,7 +2241,7 @@ $tsync = '?_t='.$t; //항상 세로 고침 되도록.
 						<div class="modal-content">
 							<div class="modal-header">
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-								<h4 class="modal-title">STROK COLOR</h4>
+								<h4 class="modal-title">STROKE COLOR</h4>
 							</div>
 							<div class="modal-body">
 								<p>One fine body&hellip;</p>
